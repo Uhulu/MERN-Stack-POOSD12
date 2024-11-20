a4e00ca9e83873f39240fce0819640f6b11096a1
Julian Mendis <49164112+pmendis02@users.noreply.github.com>
2024-11-19 23:04:26 -0500

    Add files via upload

--- server github/backend/server.js
+++ server github/backend/server.js
@@ -4,6 +4,9 @@ const express = require('express')//express package
 const mongoose = require('mongoose')//mognoose package
 
 const constelRoutes = require('./routes/constel') //grabs routes from constel.js for use
+const emailRoutes = require('./routes/signup') //grabs routes from signup.js for use
+const registerRoutes = require('./routes/user') //grabs routes from user.js for use
+
 
 //express app
 const app = express()
@@ -21,15 +24,16 @@ app.use((req,res,next)=> {
 app.use('/api/constel',constelRoutes) //path for api is /api/constel/... Whatever is in constel.js
 //eg /api/constel/ is what loads up all the consetllations
 
+app.use('/api/email',emailRoutes) //path for api is /api/email/... Whatever is in signup.js
 
-
+app.use('/api/users',registerRoutes) //path for api is /api/register/... Whatever is in user.js
 
 //connect to db
 mongoose.connect(process.env.MONGO_URI)
     .then(()=>{
         //Listen for requests once connected to db
         app.listen(process.env.PORT , () => {
-        console.log('connected to db & listening on port 4000') 
+        console.log(`connected to db & listening on port ${process.env.PORT}`) 
         })
     })
     .catch((error) => {
