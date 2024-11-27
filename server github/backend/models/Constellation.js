c037e015671363bee55942feaa7373b225255c3d
alexatuncar <al927968@ucf.edu>
2024-11-27 00:49:37 -0500

    Update Constellation.js

--- server github/backend/models/Constellation.js
+++ server github/backend/models/Constellation.js
@@ -1,30 +1,44 @@
 //schema for the data structure of a Constellations entry in MongoDB
 const mongoose = require('mongoose')
 
+
 const Schema = mongoose.Schema
 
+
 const ConstellationSchema = new Schema({
-    ItemID: {
-        type: Number,
-        required: true,
-        unique: true
-      },
-      Name: {
-        type: String,
-        required: true,
-        trim: true
-      },
-      Image: {
-        type: String,
-        required: true,
-        trim: true
-      },
-      Meaning: {
-        type: String,
-        required: true,
-        trim: true
-      }
+   ItemID: {
+       type: Number,
+       required: true,
+       unique: true
+     },
+     Name: {
+       type: String,
+       required: true,
+       trim: true
+     },
+     Image: {
+       type: String,
+       required: true,
+       trim: true
+     },
+     Meaning: {
+       type: String,
+       required: true,
+       trim: true
+     },
+     Description:{
+       type: String,
+       required: true,
+       trim: true
+     },
+     BirthMonth:{
+       type: String,
+       required: true,
+       trim: true
+     }
+
+
 })
 
-module.exports = mongoose.model('Constellation',ConstellationSchema, 'Constellations')
 
+module.exports = mongoose.model('Constellation',ConstellationSchema, 'Constellations')
