36bfd5744a3866ea833ab95fdd6620546bbb24ef
MarianaRV71 <marianar2241@gmail.com>
2024-11-28 00:57:40 -0500

    Update server.js

--- server github/backend/server.js
+++ server github/backend/server.js
@@ -2,10 +2,19 @@ require('dotenv').config()
 
 const express = require('express') //express package
 const mongoose = require('mongoose') //mogoose package
+const cors = require("cors");
+//const connection = require("./");
 const constelRoutes = require('./routes/constel') //grabs routes from constel.js for use
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
+
+//database connection
+//connection();
 
 //express app 
 const app = express()
+
+app.use(cors());
 
 //middlewear checks what requests are made and method like GET POST and shows in console
 //logs request made to backend
@@ -19,6 +28,9 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/constel', constelRoutes) //path for api is /api/constel/... Whatever is in constel.js
 //eg /api/constel/ is what loads up all the consetllations
+
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 
 //connect to db
 mongoose.connect(process.env.MONGO_URI)
