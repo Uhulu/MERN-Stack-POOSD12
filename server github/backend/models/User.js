f946d22c6687feb107911ea050576d4ba02db32a
Julian Mendis <49164112+pmendis02@users.noreply.github.com>
2024-11-30 14:01:48 -0500

    Update User.js

--- server github/backend/models/User.js
+++ server github/backend/models/User.js
@@ -1,43 +1,18 @@
 const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema;
+const userSchema = new mongoose.Schema({
+    Login: { type: String, required: true, unique: true },
+    Password: { type: String, required: true },
+    FirstName: { type: String, required: true },
+    LastName: { type: String, required: true },
+    Email: { type: String, required: true, unique: true },
+    BirthMonth: { type: String, required: true },
+    Verified: { type: Boolean, default: false }, // Automatically set to false
+    VerificationToken: { type: String, default: null }, // Automatically generate a token
+}, { timestamps: true });
 
-const UserSchema = new Schema({
-    Login: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    Password: {
-        type: String,
-        required: true
-    },
-    FirstName: {
-        type: String,
-        required: true
-    },
-    LastName: {
-        type: String,
-        required: true
-    },
-    Email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    Verified: {
-        type: Boolean,
-        default: false
-    },
-    VerificationToken: {
-        type: String
-    },
-    Favorites: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Constellation'
-        }
-    ]
-});
+// Avoid overwriting the model
+const User = mongoose.models.User || mongoose.model('User', userSchema, 'Users');
 
-module.exports = mongoose.model('User', UserSchema, 'Users');
+module.exports = User;
+
